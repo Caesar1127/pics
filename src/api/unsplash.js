b24444324917e44b2a8c00f189a3487484f8aa68
Ornell Caesar <ornell.caesar@gmail.com>
2020-06-12 15:54:27 -0400

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID _-v5LEmgbNPvjVxX90-GLR97OGkWcoAbOt0Qjy7wBAE",
+    Authorization: "Client-ID USER_API_KEY",
   },
 });
